bdb9a74fa0a0de6805827378f56e887cb1c616c1
anilstha <anilshrestha711@gmail.com>
2020-03-08 12:17:15 -0700

    Update process.php

--- process.php
+++ process.php
@@ -1,12 +1,12 @@
 &lt;?php 
- 
+
     if(isset($_POST['btn-send']))
     {
        $UserName = $_POST['UName'];
        $Email = $_POST['Email'];
        $Subject = $_POST['Subject'];
        $Msg = $_POST['msg'];
- 
+
        if(empty($UserName) || empty($Email) || empty($Subject) || empty($Msg))
        {
            header('location:index.php?error');
@@ -14,7 +14,7 @@
        else
        {
            $to = "admin@onlineittuts.com";
- 
+
            if(mail($to,$Subject,$Msg,$Email))
            {
                header("location:index.php?success");
